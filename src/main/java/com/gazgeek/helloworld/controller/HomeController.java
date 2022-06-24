5e2f883e58b4edabca0cdbf74a54e32c1b5b7a2e
Santhosh Kumar <santhoshmail2u@gmail.com>
2022-06-24 07:19:22 +0530

    modify message

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello from Santhosh, welcom to DevOps!";
+        return "Hello from Santhosh!";
     }
 
 }
